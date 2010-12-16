7b56fdceab598af2cc75332c1bf886558787614e
skrol29 <skrol29@91b6ad50-7c07-4dde-83ff-a8f70d914358>
2010-12-16 15:22:05 +0000

    update benches

--- benches/phpbench_fileexists_vs_fopen.php
+++ benches/phpbench_fileexists_vs_fopen.php
@@ -1,15 +1,21 @@
 <?php
 
+/*
+Skrol29, 2010-12-16
+http://www.tinybutstrong.com/onlyyou.html
+*/
+
 f_InfoStart('file_existe() vs @fopen()');
 
-echo "This test compares two ways of opening a file that may or may not exist.";
+f_EchoLine('Presentation','u');
+echo "This test compares two ways of opening a file that may or may not exist.<br />";
 
-// speed tests
+/* --------------
+   Speed measures
+   -------------- */
 
 f_EchoLine();
-
-/* ---------------------------------
-*/
+f_EchoLine('Speed measures','u');
 
 $b0 = f_BechThisFct('f_Nothing');
 
@@ -21,16 +27,28 @@ $b_fe_err = f_BechThisFct('f_test_file_exists',$prm_err);
 $b_of_ok  = f_BechThisFct('f_test_ofile',$prm_ok);
 $b_of_err = f_BechThisFct('f_test_ofile',$prm_err);
 
+/* ---------------
+   compare results
+   --------------- */
+
+f_EchoLine();
+f_EchoLine('Compare results','u');
+
 f_Compare("file_exists() with existing file" , $b_fe_ok, "@fopen() with existing file", $b_of_ok);
 f_Compare("file_exists() with non existing file" , $b_fe_err, "@fopen() with non existing file", $b_of_err);
 
-// end
-f_InfoEnd();
+/* ------------
+   end
+   ------------ */
+
+f_EchoLine();
+f_EchoLine('End of tests','u');
+f_InfoEnd('<a href="http://www.tinybutstrong.com">http://www.tinybutstrong.com</a>',false);
 exit;
 
-/* ---------------------------------
-   SPECIFIC FUNCTIONS AND CLASSES
-   ---------------------------------*/
+/* --------------------------------------------
+   FUNCTIONS AND CLASSES SPECIFIC TO THIS BENCH
+   -------------------------------------------- */
 
 function f_test_file_exists($file) {
 	if (file_exists($file)) {
@@ -53,7 +71,7 @@ function f_test_ofile($file) {
 }
 
 /* ---------------------------------
-   COMMON FUNCTIONS
+   COMMON FUNCTIONS (version 1.0)
    ---------------------------------*/
 
 function f_Nothing() {
@@ -71,7 +89,19 @@ function f_BechThisFct($fct, $prm=false, $nbr = 10000) {
 		$x = call_user_func_array($fct, $prm);
 	}
 	$t2 = f_Timer();
-	return ($t2-$t1);
+	$d = ($t2-$t1);
+	$av = $d/$nbr;
+	if ($av>=0.1) {
+		$av_txt = number_format($av,3,'.',',').' secconds';
+	} elseif ($av>=0.001) {
+		$av_txt = number_format(1000*$av,3,'.',',').' milli-secconds';
+	} elseif ($av>=0.000001) {
+		$av_txt = number_format(1000000*$av,3,'.',',').' micro-secconds';
+	} else {
+		$av_txt = number_format(1000000*$av,12,'.',',').' micro-secconds';
+	}
+	f_EchoLine("Bench of function '".$fct."': run ".number_format($nbr,0,'.',',')." times, average duration: ".$av_txt.".");
+	return $d;
 }
 
 function f_Timer() {
@@ -86,9 +116,14 @@ function f_Timer() {
 	return (float)$x ;
 }
 
-function f_EchoLine($x='') {
+function f_EchoLine($txt='',$conv=true) {
 // display a line of information
-	echo htmlentities($x)."<br />\r";
+	if ($conv===true) {
+		$txt = htmlentities($txt);
+	} elseif (is_string($conv)) {
+		$txt = '<'.$conv.'>'.htmlentities($txt).'</'.$conv.'>';
+	}
+	echo $txt."<br />\r";
 }
 
 function f_Compare($a_name, $a_val, $b_name, $b_val) {
@@ -104,22 +139,24 @@ function f_Compare($a_name, $a_val, $b_name, $b_val) {
 	f_EchoLine( '['.$a_name.'] is '.number_format($b_val/$a_val,2).' time faster than ['.$b_name.'] , that is a reduction of '.number_format(100*($b_val-$a_val)/$b_val,2).'% compared to ['.$b_name.'].' );
 }
 
-function f_InfoStart($Title) {
+function f_InfoStart($title) {
 // display information at the start of the test	
 	global $t_start;
 	$t_start = f_Timer();
 	
-	f_EchoLine('PHP Benches: '.$Title);
-	f_EchoLine('PHP version: '.PHP_VERSION);
-	f_EchoLine('OS type: '.PHP_OS.' ('.php_uname('s').')');
+	echo '<!DOCTYPE HTML><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>PHP Benches - '.$title.'</title></head><body>';
+	f_EchoLine('<b>PHP Benches:</b> '.htmlentities($title), false);
+	f_EchoLine('<b>PHP version:</b> '.PHP_VERSION,false);
+	f_EchoLine('<b>OS type:</b> '.PHP_OS.' ('.php_uname('s').')',false);
 	f_EchoLine();
 	
 }
 
-function f_InfoEnd() {
+function f_InfoEnd($signature=false,$conv=true) {
 // display information at the end of the test	
 	global $t_start;
 	$t_end = f_Timer();
-	f_EchoLine();
-	f_EchoLine("End of the test. Duration: ".number_format($t_end-$t_start,2)." sec.");
+	f_EchoLine("Total duration: ".number_format($t_end-$t_start,2)." sec.");
+	if ($signature!==false) f_EchoLine($signature,$conv);
+	echo '</body></html>';
 }
