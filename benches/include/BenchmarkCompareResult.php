928998469b65566c60a4043d83fc49c91b5865ea
fmancel <fmancel@91b6ad50-7c07-4dde-83ff-a8f70d914358>
2011-01-03 19:06:55 +0000

    compute error on rate correction

--- benches/include/BenchmarkCompareResult.php
+++ benches/include/BenchmarkCompareResult.php
@@ -9,7 +9,7 @@ class BenchmarkCompareResult {
 
 	function ratePrintable() {
 		$diffTime = $this->slower->totalTime - $this->faster->totalTime;
-		return number_format(($diffTime / $this->faster->totalTime) * 100, 1, '.', ' ').'%';
+		return number_format(($diffTime / $this->slower->totalTime) * 100, 1, '.', ' ').'%';
 	}
 }
 
