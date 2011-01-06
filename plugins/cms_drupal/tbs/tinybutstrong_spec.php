ebadf3943a6dd1ad5b5c2bd95386d982dcd9f0f0
skrol29 <skrol29@91b6ad50-7c07-4dde-83ff-a8f70d914358>
2011-01-06 17:44:03 +0000

    the Drupal plug-in is now working!

--- plugins/cms_drupal/tbs/tinybutstrong_spec.php
+++ plugins/cms_drupal/tbs/tinybutstrong_spec.php
@@ -1,22 +1,22 @@
 <?php
 
-/* Specific functions for the TinyButStrong plugin, can feet to the Joomla CMS.
-Version 2010-05-20
+/* Specific functions for the TinyButStrong plugin, can feet to the Drupal CMS.
+Version 2011-01-05
 */
 
 function tbs_plugin_SqlDbInit(&$TBS) {
-
-	// global variable $active_db is an instance of class mysqli
+// http://api.drupal.org/api/drupal/developer--globals.php/6
+// in Drupal, global variable $active_db is an instance of class mysqli
 	
 	global $db_url, $active_db;
-
-	$TBS->_CmsDbSrc =& $active_db;
+	$TBS->_CmsDbSrc =& $active_db; // used by MergeBlock tags
 	$TBS->_CmsDbConfigName = basename($db_url);
 	$TBS->_CmsDbActiveName = $TBS->_CmsDbConfigName;
 
 }
 
 function tbs_plugin_SqlDbId(&$DbSrc) {
+// used by MergeBlock tags
 	return $DbSrc;
 }
 
@@ -31,6 +31,7 @@ function tbs_plugin_SqlGetRows($SQL,$ErrMsg='') {
 
 function tbs_plugin_SqlExecute($SQL,$ErrMsg='',$getRows=false) {
 // Not used in the plugin, this is a wrapper for developpers
+// http://api.drupal.org/api/drupal/includes--database.mysql-common.inc/function/db_query/6
 	$result = db_query($SQL);
 	if ($result!==false) {
 		if ($getRows) {
@@ -47,11 +48,20 @@ function tbs_plugin_SqlExecute($SQL,$ErrMsg='',$getRows=false) {
 }
 	
 function tbs_plugin_GetOption($OptName, $DefaultValue) {
-	static $pluginParams = false;
-	if ($pluginParams===false) {
-		$plugin = & JPluginHelper::getPlugin('content', 'tinybutstrong');
-		$pluginParams = new JParameter($plugin->params);
-	}
-	$OptValue = $pluginParams->def($OptName,$DefaultValue);
-	return $OptValue;
+// http://api.drupal.org/api/drupal/includes--bootstrap.inc/function/variable_get/6
+	return variable_get($OptName, $DefaultValue);
+}
+
+// database plug-in for TBS
+function tbsdb_mysqli_open(&$Source, &$Query) {
+	$Rs = $Source->query($Query); // returns false if an error occurs
+	return $Rs;
+}
+function tbsdb_mysqli_fetch(&$Rs) {
+	$Rec = $Rs->fetch_array();
+	if (is_null($Rec)) $Rec = false;
+	return $Rec;
+}
+function tbsdb_mysqli_close(&$Rs) {
+	$Rs->free_result();
 }
