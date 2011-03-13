5d7ec5ab211100e8d3e234e6344b44d01827a2d6
fmancel <fmancel@91b6ad50-7c07-4dde-83ff-a8f70d914358>
2011-03-13 15:41:11 +0000

    add code coverage in unit tests report

--- trunk/testunit/index.php
+++ trunk/testunit/index.php
@@ -9,12 +9,21 @@ error_reporting(E_ALL & ~E_USER_NOTICE);
 
 // include tbs classes
 if (version_compare(PHP_VERSION,'5.0')<0)
-	@require_once(dirname(dirname(__FILE__)).'/tbs_class_php4.php');
+	$tbsFileName = dirname(dirname(__FILE__)).'/tbs_class_php4.php';
 else
-	@require_once(dirname(dirname(__FILE__)).'/tbs_class_php5.php');
+	$tbsFileName = dirname(dirname(__FILE__)).'/tbs_class_php5.php';
+@require_once($tbsFileName);
+@require_once(dirname(dirname(__FILE__)).'/plugins/tbs_plugin_html.php');
+@require_once(dirname(dirname(__FILE__)).'/plugins/tbs_plugin_bypage.php');
+@require_once(dirname(dirname(__FILE__)).'/plugins/tbs_plugin_cache.php');
+@require_once(dirname(dirname(__FILE__)).'/plugins/tbs_plugin_mergeonfly.php');
+@require_once(dirname(dirname(__FILE__)).'/plugins/tbs_plugin_navbar.php');
+// @require_once(dirname(dirname(__FILE__)).'/plugins/tbs_plugin_ref.php');
+// @require_once(dirname(dirname(__FILE__)).'/plugins/tbs_plugin_syntaxes.php');
 
 // other files required for unit tests
 require_once(dirname(__FILE__).'/include/TBSUnitTestCase.php');
+require_once(dirname(__FILE__).'/include/HtmlCodeCoverageReporter.php');
 
 // include unit test classes
 include(dirname(__FILE__).'/testcase/AttTestCase.php');
@@ -25,6 +34,7 @@ include(dirname(__FILE__).'/testcase/BlockTestCase.php');
 
 // launch tests
 ini_set("display_errors", "On");
+set_time_limit(0);
 $tbs = new clsTinyButStrong();
 $test = new GroupTest('TinyButStrong v'.$tbs->Version.' (with PHP '.PHP_VERSION.')');
 $test->addTestCase(new FieldTestCase());
@@ -32,6 +42,6 @@ $test->addTestCase(new BlockTestCase());
 $test->addTestCase(new AttTestCase());
 $test->addTestCase(new QuoteTestCase());
 $test->addTestCase(new FrmTestCase());
-$test->run(new HtmlReporter());
+$test->run(new HtmlCodeCoverageReporter(array($tbsFileName, dirname(dirname(__FILE__)).'/plugins/')));
 
 ?>
