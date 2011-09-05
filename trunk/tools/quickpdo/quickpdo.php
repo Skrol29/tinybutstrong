fc987a87614e2dae81de9092e3685422a83e1bda
skrol29 <skrol29@91b6ad50-7c07-4dde-83ff-a8f70d914358>
2011-09-05 20:58:15 +0000

    fix spelling

--- trunk/tools/quickpdo/quickpdo.php
+++ trunk/tools/quickpdo/quickpdo.php
@@ -3,8 +3,8 @@
 /*
 ********************************************************
 QuickPDO
-This class extends the very PDO objects to add shortcuts like the Zend Adaptater has.
-I wish such shorcuts were implemeted in the native PDO class.
+This class extends the very PDO objects to add shortcuts like the Zend Adapter has.
+I wish such shortcuts were implemented in the native PDO class.
 ------------------------
 Version  : 1.0 for PHP 5
 Date     : 2011-09-05
@@ -33,8 +33,8 @@ class QuickPDO extends PDO {
 	}
 
 	/* Return all records corresponding to an SQL query.
-	 * Similar to PDOStatement::fetchAll() but directly inlcudes the SQL statement.
-	 * @param {string} $sql: the sql statement
+	 * Similar to PDOStatement::fetchAll() but directly includes the SQL statement.
+	 * @param {string} $sql: the SQL statement
 	 * @param {array} $values: An array of values with as many elements as there are bound parameters in the SQL statement being executed.
 	 * @param {constant} $fetch_style: see http://www.php.net/manual/en/pdostatement.fetch.php
 	 */
@@ -48,8 +48,8 @@ class QuickPDO extends PDO {
 	}
 
 	/* Return the first record corresponding to an SQL statement.
-	 * Similar to PDOStatement::fetch() but directly inlcudes the SQL statement.
-	 * @param {string} $sql: the sql statement
+	 * Similar to PDOStatement::fetch() but directly includes the SQL statement.
+	 * @param {string} $sql: the SQL statement
 	 * @param {array} $values: An array of values with as many elements as there are bound parameters in the SQL statement being executed.
 	 * @param {constant} $fetch_style: see http://www.php.net/manual/en/pdostatement.fetch.php
 	 */
@@ -63,7 +63,7 @@ class QuickPDO extends PDO {
 	}
 
 	/* Return the first value of the first record corresponding to an SQL statement.
-	 * @param {string} $sql: the sql statement
+	 * @param {string} $sql: the SQL statement
 	 * @param {array} $values: An array of values with as many elements as there are bound parameters in the SQL statement being executed.
 	 */
 	function fetchOne($sql, $values = array() ) {
@@ -78,7 +78,7 @@ class QuickPDO extends PDO {
 	/* Return all records of an SQL statement as an associative array
 	 * where the first column of the query becomes the keys of the array, and
 	 * the second column of the query becomes the values of the array.
-	 * @param {string} $sql: the sql statement
+	 * @param {string} $sql: the SQL statement
 	 * @param {array} $values: An array of values with as many elements as there are bound parameters in the SQL statement being executed.
 	 */
 	function fetchPairs($sql, $values = array() ) {
@@ -92,8 +92,8 @@ class QuickPDO extends PDO {
 		return $result;
 	}
 
-	/* Return all values of the first colmun of an SQL statement as a indexed array.
-	 * @param {string} $sql: the sql statement
+	/* Return all values of the first column of an SQL statement as a indexed array.
+	 * @param {string} $sql: the SQL statement
 	 * @param {array} $values: An array of values with as many elements as there are bound parameters in the SQL statement being executed.
 	 */
 	function fetchCol($sql, $values = array() ) {
