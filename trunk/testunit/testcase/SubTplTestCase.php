d2086bc1aadfb098c5a718a32d5f4fc423028874
skrol29 <skrol29@91b6ad50-7c07-4dde-83ff-a8f70d914358>
2012-03-03 00:44:14 +0000

    Unit Test: add sub-templates

---
+++ trunk/testunit/testcase/SubTplTestCase.php
@@ -0,0 +1,37 @@
+<?php
+
+class SubTplTestCase extends TBSUnitTestCase {
+
+	function SubTplTestCase() {
+		$this->UnitTestCase('Sub-Template Unit Tests');
+	}
+
+	function setUp() {
+	}
+
+	function tearDown() {
+	}
+
+	function testGetPart() {
+	
+		/*
+		$blk = array();
+		$blk[] = array('id'=>'AttCache x12', 'val'=>'effect1');
+		$blk[] = array('id'=>'AttCache x13', 'val'=>'effect2');
+		$blk[] = array('id'=>'AttCache x14', 'val'=>'effect3');
+		$blk[] = array('id'=>'AttCache x15', 'val'=>'effect4');
+		*/
+		
+		// basic test
+		$this->assertEqualMergeBlockFiles('subtpl_test1_main.html', array(), 'subtpl_test1_result.html', "sub-template #1");
+
+		// getpart test
+		$this->assertEqualMergeBlockFiles('subtpl_test2_main.html', array(), 'subtpl_test2_result.html', "sub-template #2");
+
+		// store test
+		$this->assertEqualMergeBlockFiles('subtpl_test3_main.html', array(), 'subtpl_test3_result.html', "sub-template #3");
+		
+	}
+
+}
+
