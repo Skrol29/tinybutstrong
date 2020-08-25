0825df85bacbdd1dd64213b9e0e728df7653f08e
Skrol29 <skrol29dev@gmail.com>
2020-08-26 00:15:51 +0200

    [examples] replace example 'comm' with 'enlarge'

--- examples/tbs_us_examples_prmenlarge.php
+++ examples/tbs_us_examples_prmenlarge.php
@@ -3,7 +3,7 @@
 include_once('tbs_class.php');
 
 $TBS = new clsTinyButStrong;
-$TBS->LoadTemplate('tbs_us_examples_prmcomm.htm');
+$TBS->LoadTemplate('tbs_us_examples_prmenlarge.htm');
 $TBS->Show();
 
 ?>
