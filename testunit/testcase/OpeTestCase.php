bb92c750407e096b17b28afba8d7a69c4df4f9a4
Skrol29 <skrol29dev@gmail.com>
2024-05-08 02:59:43 +0200

    [testcase] ope=debug_val

--- testunit/testcase/OpeTestCase.php
+++ testunit/testcase/OpeTestCase.php
@@ -18,9 +18,9 @@ class OpeTestCase extends TBSUnitTestCase {
 		$this->assertEqualMergeFieldStrings('{[a;ope=list]}', array('a'=>array('x','y','z')),  '{x,y,z}', 'test ope=list');
 		$this->assertEqualMergeFieldStrings('{[a;ope=list;valsep=+]}', array('a'=>array('x','y','z')),  '{x+y+z}', 'test ope=list valsep=+');
 		
-		// debug
-		$this->assertEqualMergeFieldStrings('{[a;ope=debug]}', array('a'=>3128.50),  '{(double) 3128.5}', 'test debug double');
-		$this->assertEqualMergeFieldStrings('{[a;ope=debug]}', array('a'=>null),  '{(NULL) NULL}',        'test debug null');
+		// debug_val
+		$this->assertEqualMergeFieldStrings('{[a;ope=debug_val]}', array('a'=>3128.50),  '{(double) 3128.5}', 'test debug double');
+		$this->assertEqualMergeFieldStrings('{[a;ope=debug_val]}', array('a'=>null),  '{(NULL) NULL}',        'test debug null');
 		//$this->dumpLastSource(); // debug
 		
 	}
